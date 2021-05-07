df3eb231072cfaa17895d6a4f2cb69c379b14349
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-05-07 18:03:54 +0200

    Improved the test

--- src/test/java/at/jit/DmnFromCsvMainTest.java
+++ src/test/java/at/jit/DmnFromCsvMainTest.java
@@ -1,21 +1,31 @@
 package at.jit;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
 public class DmnFromCsvMainTest {
+    static final String INPUT_FILE = "src/test/resources/t1.input.csv";
+    static final String ACT_RESULT_FILE = "src/test/resources/t2.actout.dmn";
+    static final String EXP_RESULT_FILE = "src/test/resources/t2.expout.dmn";
 
     @Test
-    public void givenCsvFile_whenRun_thenReturnCorrectResult() {
+    public void givenCsvFile_whenRun_thenReturnCorrectResult() throws IOException {
         // Given
         final DmnFromCsvMain sut = new DmnFromCsvMain();
 
         // When
         sut.run(new String[] {
                 "1",
-                "src/test/resources/t1.input.csv",
-                "src/test/resources/t2.actout.dmn"
+                INPUT_FILE,
+                ACT_RESULT_FILE
         });
 
         // Then
+        assertTrue(FileUtils.contentEquals(new File(EXP_RESULT_FILE), new File(ACT_RESULT_FILE)));
     }
 }
