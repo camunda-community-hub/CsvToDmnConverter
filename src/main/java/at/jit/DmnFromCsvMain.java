95a2cb53ce4f3c17d5a0a294367fe1d0cf2e9c73
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-05-14 11:34:43 +0200

    Refactoring parsing of command-line arguments.

--- src/main/java/at/jit/DmnFromCsvMain.java
+++ src/main/java/at/jit/DmnFromCsvMain.java
@@ -1,16 +1,92 @@
 package at.jit;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.camunda.bpm.model.dmn.DmnModelInstance;
 
+import java.io.PrintStream;
+
 public class DmnFromCsvMain {
+    static final String CLI_OPTION_CSV_TO_DMN = "c";
+    static final String CLI_OPTION_DMN_TO_CSV = "d";
+    static final String CLI_OPTION_INPUT_FILE = "i";
+    static final String CLI_OPTION_OUTPUT_FILE = "o";
+    private final PrintStream sysErr;
+    private final PrintStream sysOut;
+
+    public DmnFromCsvMain() {
+        this(System.err, System.out);
+    }
+
+    DmnFromCsvMain(final PrintStream sysErr, final PrintStream sysOut) {
+        this.sysErr = sysErr;
+        this.sysOut = sysOut;
+    }
+
     public static void main(final String[] args) {
         final DmnFromCsvMain app = new DmnFromCsvMain();
         app.run(args);
     }
 
     public void run(final String[] args) {
+        final Options options = new Options();
+
+        final Option csvToDmn = Option.builder(CLI_OPTION_CSV_TO_DMN)
+                .required(false)
+                .longOpt("csv-to-dmn")
+                .desc("Convert CSV to DMN")
+                .build();
+
+        final Option dmnToCsv = Option.builder(CLI_OPTION_DMN_TO_CSV)
+                .required(false)
+                .longOpt("dmn-to-csv")
+                .desc("Convert DMN to CSV")
+                .build();
+
+        final Option inputFileOpt = Option.builder(CLI_OPTION_INPUT_FILE)
+                .required(true)
+                .longOpt("input-file")
+                .desc("Input file (CSV or DMN)")
+                .build();
+
+        final Option outputFileOpt = Option.builder(CLI_OPTION_OUTPUT_FILE)
+                .required(true)
+                .longOpt("output-file")
+                .desc("Output file (CSV or DMN)")
+                .build();
+
+        options.addOption(csvToDmn);
+        options.addOption(dmnToCsv);
+        options.addOption(inputFileOpt);
+        options.addOption(outputFileOpt);
+
+        final CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = null;
+        try {
+            commandLine = parser.parse(options, args);
+        } catch (final ParseException e) {
+            sysErr.println(String.format("Invalid input: %s", e.getMessage()));
+            printUsageText(options);
+            return;
+        }
+
+        if (!commandLine.hasOption(CLI_OPTION_DMN_TO_CSV) &&
+                !commandLine.hasOption(CLI_OPTION_CSV_TO_DMN)) {
+            sysErr.println("Mode (CSV to DMN, DMN to CSV) not specified");
+            printUsageText(options);
+            return;
+        }
+
+        // TODO: Check the validity of input and output files
+
+
         if (args.length != 3) {
-            System.out.println("Usage: java -jar CsvToDmnConverter mode input-file output-file");
+            sysErr.println("Usage: java -jar CsvToDmnConverter mode input-file output-file");
         }
 
         final String mode = args[0].trim();
@@ -18,7 +94,7 @@ public class DmnFromCsvMain {
         final String inputFile = args[1];
         final String outputFile = args[2];
         if (!new FileExtensionsValidator().fileExtensionsValid(inputFile, outputFile, mode)) {
-            System.out.println("One of the entered file extensions is wrong, exiting program..");
+            sysErr.println("One of the entered file extensions is wrong, exiting program..");
             return;
         }
 
@@ -27,7 +103,7 @@ public class DmnFromCsvMain {
         } else if (Modes.DMN_TO_CSV.equals(mode)) {
             convertDmnToCsv(inputFile, outputFile);
         } else {
-            System.out.println("Entered option is not 1 or 2, exiting program..");
+            sysErr.println("Entered option is not 1 or 2, exiting program..");
             return;
         }
     }
@@ -46,5 +122,11 @@ public class DmnFromCsvMain {
 
         final DmnFileExporter dmnFileExporter = new DmnFileExporter();
         dmnFileExporter.exportToDmnFile(dmnModelInstance, outputFile);
+    }
+
+    void printUsageText(final Options options) {
+        final HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar csv2dmn-converter.jar",
+                options);
     }
 }
